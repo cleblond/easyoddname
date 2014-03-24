6416ad3f38677ff74966a1c892347adcb3db925a
Carl LeBlond <carl@easyochem.com>
2014-03-24 11:18:49 -0400

    Update version.php

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
  *
  * @package    qtype
  * @subpackage easyoddname
- * @copyright  2014 onwards Carl LeBlond {@link http://www.scimersion.com}
+ * @copyright  2014 onwards Carl LeBlond 
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
